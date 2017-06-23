a3af578c5c366b450858ca86e299e28707c7a296
Michelle Gardner <mgm7c@mail.umkc.edu>
2017-06-22 23:38:11 -0500

    Created the GUI, started on checking if JTextFields are empty. Add Courses Button works. Have an JOption box appear if JTextFields are empty.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import jframes.MainJFrame;
-
-import javax.swing.*;
+import Views.MainWindow;
 
 /**
  * Created by mgard on 6/21/2017.
@@ -9,11 +7,13 @@ public class Main {
 
    //TODO: bring up starting frame.
 
-    private static JFrame MainJFrame = new MainJFrame("Courses");
-
     public static void main(String[] args){
         System.out.println("Application Started");
-        MainJFrame.setVisible(true);
+
+
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.setVisible(true);
+
     }
 
 }
--- src/Views/MainWindow.java
+++ src/Views/MainWindow.java
@@ -1,31 +1,90 @@
 package Views;
 
+import javafx.scene.control.Alert;
+import javafx.scene.effect.DropShadow;
+
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import static javafx.scene.paint.Color.BLACK;
 
 /**
  * Created by mgard on 6/22/2017.
  */
-public class MainWindow {
+public class MainWindow extends JFrame {
     private JPanel panel1;
-    private JButton buttonMSG;
+    private JButton addCoursebutton;
+    private JComboBox departmentCombobox;
+    private JTextField courseCodeTextField;
+    private JTextField courseNameTextField;
+    private JTextField creditsTextField;
+    private JButton displayAllButton;
+    private JButton displayByDepartmentsButton;
+    private JTextArea listTextArea;
 
     public MainWindow() {
-        buttonMSG.addActionListener(new ActionListener() {
+        this.setTitle("Courses");
+
+        // Setup what happens when you click the close button
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Set JFrame size
+        this.setSize(500, 200);
+
+        // disable window resizing
+        this.setResizable(false);
+
+        // place planel1 object which holds all the content
+        this.setContentPane(panel1);
+
+//        frame.setBounds(new EmptyBorder(10,10,10,10));
+
+        // Window shadow
+        DropShadow border = new DropShadow();
+        border.setColor(BLACK);
+        border.setOffsetX(150);
+
+        addCoursebutton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e){
+
+                String getCoursecode = courseCodeTextField.getText();
+                String getCourseName = courseNameTextField.getText();
+                int getNumOfCreds = creditsTextField.;
+                String getDepartment = departmentCombobox.getSelectedItem().toString();
+
+                // TODO: add in credits. 
+                if(courseCodeTextField.getText().isEmpty() || courseCodeTextField.getText().isEmpty() || ){
+                    JOptionPane.showMessageDialog(null, "Need to fill in all boxes.");
+                }else{
+                    JOptionPane.showMessageDialog(null, "Added Course.");
+                    //TODO: Add courses to list.
+                    // TODO: make sure Credits are ints, 1-5
+                }
+
+
+
+
+                }
+            });
+
+
+        displayAllButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Help me.");
+
             }
         });
-    }
+        displayByDepartmentsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("app");
-        frame.setContentPane(new MainWindow().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+            }
+        });
     }
 
     private void createUIComponents() {
